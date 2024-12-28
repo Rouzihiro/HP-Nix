f0a91d96d9e34cd36d170de3959a89ab0e69210c
Rouzihiro <ryossj@gmail.com>
2024-12-28 14:08:26 +0100

    alias-optimization, preparing gaming: host

--- home-manager/shells/zsh.nix
+++ home-manager/shells/zsh.nix
@@ -1,11 +1,10 @@
-{ config, ... }:
+{ ... }:
 let
   myAliases = {
     ls = "eza --icons --grid --all --color=always";
     la = "eza --icons -l -T -L=1";
     cat = "bat";
     hypr-config = "nvim ~/.config/hypr/keybinds.conf";
-    #neodir = "cd ~/.config/nvim";
     v = "nvim";
     sv = "sudo nvim";
     zsource = "source ~/.zshrc";
@@ -15,8 +14,13 @@ let
     xx = "exit";
     cd = "z";
     ".." = "cd ..";
+    "..." = "cd ../..";
+    ".3" = "cd ../../..";
+    ".4" = "cd ../../../..";
+    ".5" = "cd ../../../../..";
     md = "mkdir -pv";
     HP = "sudo nixos-rebuild switch --flake .#HP";
+    Gaming = "sudo nixos-rebuild switch --flake .#Gaming";
     crp = "rsync -ah --progress";
     clean = "sudo nix-collect-garbage -d && sudo nix-env --delete-generations old";
     wipe = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system";
@@ -27,34 +31,6 @@ let
     df = "duf -hide special";
     mem = "free -h";
     ko = "pkill";
-
-    g = "git";
-    #ga = "git add ."; # Stage all changes
-    #gc = "git commit -m"; # Commit with a message
-    #gca="git commit --amend";  # Amend the last commit
-    #gp = "git push origin flakes"; # main"; # Push changes
-    #gpl = "git pull"; # Pull latest changes
-    #gst = "git status"; # Show git status
-    #gsb = "git checkout"; # Switch branches
-    #gcb = "git checkout -b"; # Create a new branch
-    #gsc = "git diff"; # Show changes
-    #gss = "git diff --staged"; # Show staged changes
-    #gl = "git log --oneline --graph --all --decorate"; # Pretty log
-    #gcl="git clone";           # Clone a repo
-    #gr="git restore";          # Restore changes in working directory
-    #grs="git restore --staged"; # Unstage changes
-    #grb="git rebase";          # Start a rebase
-    #grbi="git rebase -i";      # Interactive rebase
-    #gb="git branch";           # List branches
-    #gba="git branch -a";       # List all branches (local + remote)
-    #gm="git merge";            # Merge branches
-    #gs???="git stash save";      # Stash changes
-    #gsp="git stash pop";       # Apply stashed changes
-    #gsa="git stash apply";     # Apply without removing from stash
-    #gsl="git stash list";      # List stashes
-    #gsta="git stash drop";     # Drop a stash
-    #gpr="git pull --rebase";   # Pull with rebase
-
   };
 in {
   programs.zsh = {
