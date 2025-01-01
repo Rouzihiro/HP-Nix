51a46ad6a59d4784c5845e8f359809782866391b
Rouzihiro <ryossj@gmail.com>
2025-01-01 15:09:04 +0100

    Starting a new sway branch on main

--- hosts/HP/greetd.nix
+++ hosts/HP/greetd.nix
@@ -2,13 +2,14 @@
 let
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
   hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
+  sway-session = "${pkgs.sway}/share/wayland-sessions";
 in {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
         command =
-          "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
+          "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}:${sway-session}";
         user = "rey";
       };
     };
